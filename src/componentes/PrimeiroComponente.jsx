cf4f9ec17cf2948c813b9455a38e7f903360ba3d
Rafaela Polizel <polizelc.rafaela@gmail.com>
2019-05-13 18:41:42 -0300

    componentes

---
+++ src/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//arrow function
+export default () => <h1>Primeiro Componente!</h1>
+
+
+//função anônima
+// export default function(){
+//     return <h1>Primeiro Componente!</h1>
+// }
+
+
+// function primeiro(){
+//     return <h1>Primeiro Componente!</h1>
+// }
+
+// export default primeiro
--- src/index.js
+++ src/index.js
@@ -36,21 +36,32 @@
 
 
 //com JSX
-import ReactDOM from 'react-dom'
-import React from 'react'
+// import ReactDOM from 'react-dom'
+// import React from 'react'
 
-const element = document.getElementById('root')
+// const element = document.getElementById('root')
 
-ReactDOM.render(
-    <ul>
-        <li>Pedro</li>
-        <li>Ana</li>
-        <li>Maria</li>
-    </ul>
-    , element)
+// ReactDOM.render(
+//     <ul>
+//         <li>Pedro</li>
+//         <li>Ana</li>
+//         <li>Maria</li>
+//     </ul>
+//     , element)
 
 //Quando tem-se uma Single Page Aplication tem-se uma única página na aplicação (public -> index.html). Como tem-se apenas uma única página,
 //todo conteúdo (inclusive o html) gerado será a partir do JS. Utilizando o JSX, o conteúdo é gerado de forma mais simples, sem necessariamente precisar interagir com métodos e mais métodos para manipular a DOM
 
 
+import PrimeiroComponente from './componentes/PrimeiroComponente'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const elemento = document.getElementById('root')
+
+ReactDOM.render(
+    <div>
+        <PrimeiroComponente></PrimeiroComponente>   
+    </div>
+    , elemento)
 
